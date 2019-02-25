27f9ac100969c92f58e80d41a5ffb134bb25414e
Jaisel Rahman <jaiselrahman@gmail.com>
2019-02-25 20:12:48 +0530

    Add play icon

--- app/src/main/java/com/jaiselrahman/wastatus/ui/downloads/DownloadsFragment.kt
+++ app/src/main/java/com/jaiselrahman/wastatus/ui/downloads/DownloadsFragment.kt
@@ -2,7 +2,6 @@ package com.jaiselrahman.wastatus.ui.downloads
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -99,7 +98,6 @@ class DownloadsFragment : Fragment() {
 
     private fun showTapTargets() {
         if (videoList.size <= 0 || App.isShownTapTargetForDownloads) return
-        Log.i(App.TAG, "Show")
         TapTargetSequence(activity).targets(
             TapTarget.forView(
                 videoList[0].rootView.split,
